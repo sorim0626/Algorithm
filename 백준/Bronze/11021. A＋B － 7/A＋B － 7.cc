09200638b18fafe291ee6c11773c37d7b6d3736d
sorim0626 <165128798+sorim0626@users.noreply.github.com>
2024-06-22 22:44:53 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int t;
+    int a, b;
+
+    scanf("%d", &t);
+
+    for (int i = 1; i <= t; i++) {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d \n", i, a + b);
+    }
+
+    return 0;
+}
