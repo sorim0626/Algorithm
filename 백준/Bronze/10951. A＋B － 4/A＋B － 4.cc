87a6a99b1ead22f1969bee1cf4c44e06864bc6d2
sorim0626 <165128798+sorim0626@users.noreply.github.com>
2024-06-22 23:07:04 +0900

    A＋B － 4.cc

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -2,7 +2,7 @@
 
 int main(void) {
     int a,b;
-    while(scanf("%d %d", &a, &b)!=EOF) {
-        printf("%d\n", a+b);
+    while(scanf("%d %d", &a, &b)!=EOF) {  // EOF는 End Of File의 약자 
+        printf("%d\n", a+b);              // 파일의 끝을 표현하기 위한 상수( -1의 값을 가짐)
     }
 }
