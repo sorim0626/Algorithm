72bb37d8aaf5f739fc392f42ca38b5eb40590940
sorim0626 <165128798+sorim0626@users.noreply.github.com>
2024-06-26 15:12:52 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,12 @@
+int main()
+{
+
+	printf("         ,r\'\"7\n");
+	printf("r`-_   ,\'  ,/\n");
+	printf(" \\. \". L_r\'\n");
+	printf("   `~\\/\n");
+	printf("      |\n");
+	printf("      |\n");
+
+	return 0;
+}
