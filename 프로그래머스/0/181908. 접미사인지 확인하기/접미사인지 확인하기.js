dd346b7ac8b69cb3677af71405ca71d126929068
sorim0626 <165128798+sorim0626@users.noreply.github.com>
2024-09-13 12:22:12 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,9 +1,11 @@
 function solution(my_string, is_suffix) {
-    var answer = 0;
-    if(my_string.endsWith(is_suffix))
+    var answer = 1;
+    for(let i = 0; i < is_suffix.length; i++)
         {
-            answer = 1;
+            if(my_string[my_string.length - is_suffix.length + i] !== is_suffix[i])
+                {
+                    return 0;
+                }
         }
-    
     return answer;
 }
