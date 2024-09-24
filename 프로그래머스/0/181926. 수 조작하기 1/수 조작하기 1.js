fd614f56ac7e668eea9e40b18387ca78d28f1064
sorim0626 <165128798+sorim0626@users.noreply.github.com>
2024-09-24 15:47:10 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.70 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,24 +1,22 @@
-function solution(n, control) {
-    var answer = 0;
+function solution(n, control) {    
     for(let i = 0; i < control.length; i++)
         {
-            if(control[i] == "w")
+            if(control[i] == 'w')
                 {
                     n += 1;
                 }
-            else if(control[i] == "s")
+            else if(control[i] == 's')
                 {
                     n -= 1;
                 }
-            else if(control[i] == "d")
+            else if(control[i] == 'd')
                 {
                     n += 10;
                 }
-            else if(control[i] == "a")
+            else if(control[i] == 'a')
                 {
                     n -= 10;
                 }
         }
-    answer = n;
-    return answer;
+    return n;
 }
