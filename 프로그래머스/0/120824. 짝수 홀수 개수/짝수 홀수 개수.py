e5cb106c71d8c001cf391723f3fd96217fc2fefc
sorim0626 <165128798+sorim0626@users.noreply.github.com>
2024-11-10 22:11:50 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = []
+    even = 0
+    odd = 0
+    for i in num_list:
+        if i % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    answer = [even, odd]
+    return answer
