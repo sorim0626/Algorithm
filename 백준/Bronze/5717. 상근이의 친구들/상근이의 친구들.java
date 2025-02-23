6349e83f47f01f5e149ce62bfeeac45d95da993f
sorim0626 <165128798+sorim0626@users.noreply.github.com>
2025-02-23 15:22:53 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 172 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int m = sc.nextInt();
+            int f = sc.nextInt();
+
+            if (m == 0 && f == 0) {
+                break;
+            }
+
+            System.out.println(m + f);
+        }
+    }
+}
